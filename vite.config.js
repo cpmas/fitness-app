4536e07671e939d70e212ac2a41cdd3601413cf9
cpmas <calemason11@gmail.com>
2026-06-25 20:00:36 +1000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Replace 'fitness-app' with your repo name if you named it differently
+  base: '/fitness-app/', 
 })
